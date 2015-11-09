ced9ac3934055b6d168eb55f1769103d921c6f5a
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-11-09 13:31:46 +0000

    [build_lzcomp.py] read source from .c file; change signature of do_compress

--- src/python/build_lzcomp.py
+++ src/python/build_lzcomp.py
@@ -3,87 +3,21 @@ import os
 
 
 CURR_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-MODULE_NAME = "_lzcomp_cffi"
-
+with open(os.path.join(CURR_DIR, "..", "c", "_lzcompmodule.c"), 'r') as f:
+    source = f.read()
 
 ffi = cffi.FFI()
 
-
 ffi.cdef("""
-uint8_t* do_compress(long length, uint8_t *input,
-                     long *p_output_length);
+int do_compress(long length, uint8_t *input,
+                long *p_output_length, uint8_t *output);
 uint8_t* do_decompress(long length, uint8_t* input,
                        long* p_output_length);
 """)
 
-
-ffi.set_source('lzcomp.'+MODULE_NAME, """
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <config.h>
-#include <mtxmem.h>
-#include <bitio.h>
-#include <ahuff.h>
-#include <lzcomp.h>
-#include <errcodes.h>
-
-uint8_t* do_compress(long length, uint8_t* input,
-                     long* p_output_length) {
-    int returned_status = 0;
-    MTX_MemHandler* mem = NULL;
-    LZCOMP* lzcomp = NULL;
-    uint8_t *output = NULL;
-
-    mem = MTX_mem_Create(&malloc, &realloc, &free);
-    if (!mem) goto CLEANUP;
-
-    lzcomp = MTX_LZCOMP_Create1(mem);
-    if (!lzcomp) goto CLEANUP;
-
-    output = (uint8_t*)MTX_LZCOMP_PackMemory(
-        lzcomp, input, length, p_output_length);
-    if (!output) {
-        goto CLEANUP;
-    } else {
-        MTX_LZCOMP_Destroy(lzcomp);
-        free(mem);
-        return output;
-    }
-CLEANUP:
-    MTX_LZCOMP_Destroy(lzcomp);
-    free(mem);
-    return NULL;
-}
-
-uint8_t* do_decompress(long length, uint8_t* input,
-                       long* p_output_length) {
-    int returned_status = 0;
-    MTX_MemHandler* mem = NULL;
-    LZCOMP* lzcomp = NULL;
-    uint8_t *output = NULL;
-
-    mem = MTX_mem_Create(&malloc, &realloc, &free);
-    if (!mem) goto CLEANUP;
-
-    lzcomp = MTX_LZCOMP_Create1(mem);
-    if (!lzcomp) goto CLEANUP;
-
-    output = (uint8_t*)MTX_LZCOMP_UnPackMemory(
-        lzcomp, input, length, p_output_length, 3);
-    if (!output) {
-        goto CLEANUP;
-    } else {
-        MTX_LZCOMP_Destroy(lzcomp);
-        free(mem);
-        return output;
-    }
-CLEANUP:
-    MTX_LZCOMP_Destroy(lzcomp);
-    free(mem);
-    return NULL;
-}
-""",
+ffi.set_source(
+    'lzcomp._lzcomp_cffi',
+    source,
     source_extension='.c',
     sources=[
         "src/c/ahuff.c",
@@ -100,7 +34,7 @@ CLEANUP:
         "src/c/errcodes.h",
     ],
     include_dirs=["src/c"]
-  )
+    )
 
 
 if __name__ == '__main__':
